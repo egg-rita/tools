d6e2268673b356f45ffbd5a258ac5d3418ec82f0
赵凯乐 <zhaokaile@zhaokailes-MacBook-Pro.local>
2018-03-29 14:52:29 +0800

    Initial Commit

---
+++ ToolDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ToolDemo
+//
+//  Created by 赵凯乐 on 2018/3/29.
+//  Copyright © 2018年 赵凯乐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
